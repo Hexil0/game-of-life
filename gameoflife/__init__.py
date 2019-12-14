e498736111b64eb83b182764cbcfcd6d7c1b8e49
Hex <hexill@tlen.pl>
2019-12-14 14:34:43 +0100

    version of __init__

--- gameoflife/__init__.py
+++ gameoflife/__init__.py
@@ -1,5 +1,3 @@
-
-
 class GameOfLife:
 
     def __init__(self, seed):
@@ -13,16 +11,21 @@ class GameOfLife:
         pass
 
     def die(self, cell):
-        pass
+        self._state.remove(cell)
 
     def live(self, cell):
-        pass
+        self._state.add(cell)
 
     def is_alive(self, cell):
-        pass
+        return cell in self._state
 
     def get_neighbors_count(self, cell):
-        pass
+        cell_x, cell_y = cell
+        neighbors = [self.is_alive((cell_x + x, cell_y + y))
+                     for x, y in
+                     zip((-1, 1, 1, 1, 0, 0, 1, -1),
+                         (-1, 1, -1, 1, -1, 1, 0, 0))]
+        return sum(neighbors)
 
     def __str__(self):
         pass
